b1d2a607f80431612de4faca85b24a57344ab290
jojonari <xkgun@naver.com>
2017-06-18 22:45:58 +0900

    3장 스프링 부트 자동 구성과 주요기능 완료

--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
  실전 스프링 부트 워크북
 =============================
 
-### 2장 스프링 부트 앱 처녀 개발
-
 > 도서 : 실전 스프링 부트 워크북 - 한빛 미디어
+
+### 2장 스프링 부트 앱 처녀 개발
 
 1. 스프링 부트 CLI설치()
     - 리눅스, macOS, 솔라리스(macOS기준으로 설명)
@@ -47,3 +47,48 @@
         
         >스프링 부트는 @SpringBootApplication과 그 내부의자동구성(@EnableAutoConfiguration) 기능 덕분에 전체 애플리케이션 컴포넌트를 식별합니다 우선 클래스 패스를 조사해서 spring-boot-starter-web 스타터가 선언된 것을 인지한스프링부트는 웹애플리케이션을 구성합니다 .따라서 @RequestMapping이 달린 메서드가 있고 @Controller 애너테이션이 달린 클래스는 웹컨트롤러로 보고 톰켓 서버는 spring-boot-starter-web의 의존체 중 하나이므로 톰캣 서버를 띄웁니다    
   
+  
+### 3장 스프링부트 자동구성과 주요기능
+
+1. 3.1 자동구성
+    - 스프링부트의 핵심기능은 (메이븐이나 그레이들의 파일내용에 따라) 클래스패스, 애너테이션 기타 자바 구성 클래스를 보고 적합한 앱으로 맞춤하는 자동 구성이다.
+    - 앱 실행전에 긍정대조와 부정대조를 통해 스프링 부트는 적합한 앱으로 구성한다.
+    - 특정 자동 구성 끄기
+        * 자동 구성을 끄고 싶으면 @EnableAutoConfiguration의 exclude 파라미터 속성값에 클래스를 명시하면 된다
+        * @SpringBootApplication은 @EnableAutoConfiguration, @Configuration, @ComponentScan을 상속한 애너테이션이라 @SpringBootApplication에 exclude파라미터를 쓸수 있다.
+2. 3.2 EnableAutoConfiguration과 @Enable<기술명>
+    - @EnableAutoConfiguration은 앱에서 필요한 빈을 유추해서 구성하는 클래스
+        * 자동구성 클래스는 글래스패스 및 앱에 정의한 빈에 따라 적용 여부 결정
+3. 3.3 스프링부트의 주요기능
+    - 스프링부트는 실행할 앱의 타입을 유추해서 자동구성하고 어떻게 해서 무엇을 보여줄지 어떤 기능을 켜고 끌지 프로퍼티값으로 커스터마이징 가능한 고도의 맛춤형 프레임워크다
+    - 프로젝트 생성
+        * `spring init -g=com.fast87.spring -a=spring-boot-simple --package=com.fast87.spring -name=spring-boot-simple -x`
+    - @SpringBootAplication
+        * @EnableAutoConfiguration, @Configuration, @ComponentScan의 합본
+        * SpringApplication은 main 메서드에서 실행할 스프링부트 앱의 부트스트랩 역할을 한다. 실행 클래스를 인자로 넘긴다
+    - src/main/resource/banner.txt 파일을 실행 배너로 사용할 수 있다.
+        * spring.main.banner-mode=off로 배너를 끌 수 있다.
+    - 모든 웹 구성은 메인 스프링 컨텍스트에 의존하므로 반드시 자식으로 선언하고, 부모와 자식 모두 필히 동일한 org.springframework.core.Environment인터페이스를 공유해야한다
+    - 스프링 부투 앱에 인자를 넘기, 빈에서 args에 접근할 수 있다.
+    - 메이븐 빌드 : `./mvnw package`
+    - jar 실행 : `java -jar jar파일명`
+    - 인자전달 : `java -jar jar파일명 --containsOption인자명 NonOption인자1 NonOption인자2 `
+    - 디버그 모드 : `./mvnw spring-boot:run -Drun.arguments="인자1,인자2""`(,로 인자 구분)
+4. 3.4 애플리케이션 구성
+    - 스프링부트에서 앱 구성 저장
+        * 클래스패스에 application.properties파일을 둔다.(다른 곳에 추가할 수도 있다)
+        * YAML표기법에 따라 작성한 application.yml 파일을 클래스 패스에 둔다.(다른 곳에 추가할 수도 있다)
+        * 환경변수를 사용한다 클라우드 환경에선 이방법이 기본이다.
+        * 터미널 창에 인자로 전달한다.
+     - @Value("프로퍼티명")으로 값을 가져올수 있다.
+     - 프로퍼티를 찾는 순서
+        * 현재 폴더 하위의 /config폴더
+        * 현재 폴더
+        * 클래스패스 /config 패키지
+        * 클래스패스 루트
+     - 스프링부트는 바인딩을 느슨하게 처리
+        * server.ip = SERVER_IP 처럼 표기법이 달라도 인식
+     - @Component @ConfigurationProPerties(prefix="myapp")
+        * @ConfigurationProperties는 이 클래스가 application.properties파일에 정의된 모든 프로퍼티 앞에 접두어 myapp을 붙였다고 스프링 부트에 알리는 애너테이션 입니다. 그래서 myapp.serverIp, myapp.name, myapp.description프로퍼티를 올바르게 인식합니다.
+        * @Component는 스프링 빈 클래스입을 표시하는 애너테이션입니다.
+        
--- src/main/java/com/fast87/journal/SpringBootJournalApplication.java
+++ src/main/java/com/fast87/journal/SpringBootJournalApplication.java
@@ -1,22 +1,131 @@
 package com.fast87.journal;
 
-import com.fast87.journal.domain.Journal;
-import com.fast87.journal.repository.JournalRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @SpringBootApplication
 public class SpringBootJournalApplication {
+    private static final Logger log = LoggerFactory.getLogger(SpringBootJournalApplication.class);
 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJournalApplication.class);
+    }
+    @Value("${myapp.server-ip}")
+    String serverIp;
+
+    @Autowired
+    MyAppProperties props;
+
+    @Bean
+    CommandLineRunner values(){
+        return args -> {
+            log.info(">>> 서버 IP : " + serverIp);
+            log.info(">>> 앱이름 : " + props.getName());
+            log.info(">>> 앱 정보 : " + props.getDescription());
+
+        };
+    }
+
+    @Component
+    @ConfigurationProperties(prefix = "myapp")
+    public static class MyAppProperties{
+        private String name;
+        private String description;
+        private String serverIp;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getServerIp() {
+            return serverIp;
+        }
+
+        public void setServerIp(String serverIp) {
+            this.serverIp = serverIp;
+        }
+    }
+
+    /*
+    //클래스버전 2
+    private static final Logger log = LoggerFactory.getLogger(SpringBootJournalApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJournalApplication.class);
+    }
+
+    @Bean
+    String info(){
+        return "그냥 간단한 문자열 반환";
+    }
+
+    @Autowired
+    String info;
+
+    @Bean
+    CommandLineRunner myMethod(){
+        return args -> {
+            log.info("## >> CommandLineRunner 구현체...");
+            log.info("## >> info 빈에 엑세스. " + info);
+            for (String arg:args)
+                log.info(arg);
+        };
+    }*/
+
+    /*//클래스 버전 11
+public class SpringBootJournalApplication implements CommandLineRunner, ApplicationRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringBootJournalApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJournalApplication.class);
+    }
+
+    @Bean
+    String info(){
+        return "그냥 간단한 문자열 반환";
+    }
+
+    @Autowired
+    String info;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("## >> ApplicationRunner 구현체..");
+        log.info("## >> info 빈에 엑세스. " + info);
+        args.getNonOptionArgs().forEach(file -> log.info(file));
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("## >> CommandLineRunner 구현체...");
+        log.info("## >> info 빈에 엑세스. " + info);
+        for (String arg:args)
+            log.info(arg);
+    }*/
+
+    /*
+    // 클래스 버전 10
 	@Bean
 	InitializingBean saveData(JournalRepository repo){
 		return ()->{
@@ -53,5 +162,7 @@ public class SpringBootJournalApplication {
 			}
 		}
 
-	}
+	}*/
+
+
 }
